d880fbe6517152ef13174d3d03729ebb456a2d81
akaRicane <philippe.guelen@gmail.com>
2022-06-13 02:57:00 +0200

    light control work

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 import os, sys
+import socketio
 
 sys.path.append(os.getcwd())
 from dmxlib.DxmUniverse import DmxUniverse
+
+# instanciate socket
+sio = socketio.Client()
 
 # instanciate universe
 universe = DmxUniverse('/dev/tty.usbserial-EN305173')
 
 # add light to universe (start dmx channel & n-channels)
 universe.addLight(1, 4)
-universe.addLight(5, 4)
 
-# set values to channels of queried light
-universe.setValuesOfLight(0, [10, 0, 255, 255])
-universe.setValuesOfLight(1, [255, 0, 255, 255])
+@sio.event
+def connect():
+    print('connection established')
 
-# render universe
-universe.render(duration=2)
 
-# close
-universe.closeSession()
+@sio.on('dmx-request')
+def handle_dmx_query(dmx_query):
+    print(f"{dmx_query=}")
+    dimmer = dmx_query['dimmer']
+    red = dmx_query['r']
+    green = dmx_query['g']
+    blue = dmx_query['b']
+    universe.setValuesOfLight(0, [dimmer, red, green, blue])
+    universe.render(duration=0.1)
+
+
+@sio.on('mouse')
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+
+@sio.event
+def disconnect():
+    print('closing lights')
+    universe.closeSession()
+    print('disconnected from server.')
+    # close
+
+
+sio.connect('http://localhost:3001')
+sio.wait()
